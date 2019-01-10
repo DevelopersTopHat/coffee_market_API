4d125e89f988c339ed8391702878156a761b6893
DevelopersTopHat <abdeali1997@gmail.com>
2019-01-09 20:44:12 -0500

    added the ability to post new products to database

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -5,6 +5,25 @@ module Api
                 products = Product.order('created_at DESC');
                 render json: {status: 'SUCCESS', message: 'Loaded coffee products', data: products}, status: :ok
             end
+
+            def show
+                product = Product.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded coffee product', data: product}, status: :ok
+            end
+
+            def create
+                product = Product.new(product_params)
+
+                if (product.save)
+                    render json: {status: 'SUCCESS', message: 'Saved new coffee product', data: product}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'New coffee product not saved', data: product.errors}, status: :unprocessable_entity
+                end
+            end
+            private 
+                def product_params
+                    params.permit(:title, :price, :inventory_count, :description)
+                end
         end
     end
 end
